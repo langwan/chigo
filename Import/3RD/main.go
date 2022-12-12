522dce6ebc54ba9bc25b3d44c2af148d8c610bf7
langwanluo <langwnaluo@gmail.com>
2022-12-13 00:45:56 +0800

    add import

--- Import/3RD/main.go
+++ Import/3RD/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	helper_gen "github.com/langwan/langgo/helpers/gen"
+	"github.com/langwan/langgo/helpers/gen"
 )
 
 func main() {
---
+++ Import/Local/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/langwan/chigo/Import/Local/utils"
+)
+
+func main() {
+	foo := utils.Foo(1, 2)
+	fmt.Println(foo)
+}
