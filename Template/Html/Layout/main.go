6cfe9458ed020ff0edc54b55cab4241c1fd93287
langwanluo <langwnaluo@gmail.com>
2022-12-24 22:31:50 +0800

    add

---
+++ Template/Html/Layout/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Data struct {
+	Name string
+}
+
+func main() {
+	data := Data{Name: "chihuo"}
+	tmpl, err := template.ParseFiles("./index.tmpl")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(os.Stdout, data)
+}
